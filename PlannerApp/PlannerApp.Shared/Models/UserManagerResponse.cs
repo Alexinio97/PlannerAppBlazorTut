645cbda5443dc4b0a8fb91debabb39bf8eada161
Andricsak Alexandru <38785216+Alexinio97@users.noreply.github.com>
2020-10-24 22:35:48 +0300

    Added all project

---
+++ PlannerApp/PlannerApp.Shared/Models/UserManagerResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlannerApp.Shared.Models
+{
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+        public bool IsSucces { get; set; }
+        public string[] Errors { get; set; }
+        public DateTime? ExpiredDate { get; set; }
+    }
+}
---
+++ PlannerApp/PlannerApp.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AKSoftware.WebApi.Client;
+using PlannerApp.Shared.Models;
+
+namespace PlannerApp.Shared.Services
+{
+    public class AuthenticationService
+    {
+        private readonly string _baseUrl;
+
+        ServiceClient _client = new ServiceClient();
+
+        public AuthenticationService(string url)
+        {
+            _baseUrl = url;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            try
+            {
+                var response = await _client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/register", request);
+                return response.Result;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception caught: " + ex.ToString());
+                return null;
+            } 
+        }
+    }
+}
